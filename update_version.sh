3100f362e997633e4df1e4912d72dca04436b5d1
Alexey Vishnyakov <vishnya@ispras.ru>
2022-10-19 14:49:38 +0300

    Add update version script

---
+++ update_version.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+    echo Usage: $0 old_version new_version
+    echo Example: $0 1.2.0 1.3.0
+fi
+
+sed -i "s/version = \"$1\"/version = \"$2\"/g" Cargo.toml
+sed -i "s/\"$1\"/\"$2\"/g" src/bin/casr.rs
+sed -i "s/\"$1\"/\"$2\"/g" src/bin/casr-cli.rs
+sed -i "s/\"$1\"/\"$2\"/g" src/bin/casr-cluster.rs
+sed -i "s/\"$1\"/\"$2\"/g" src/bin/casr-san.rs
+sed -i "s/\"$1\"/\"$2\"/g" src/bin/casr-gdb.rs
