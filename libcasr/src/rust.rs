549d325863916717b08cfbba368dcc40f2d078b9
Georgy Savidov <60780909+Avgor46@users.noreply.github.com>
2023-08-02 19:53:25 +0300

    Fix - support new Rust panic message format where message is on new line (#113)

--- libcasr/src/rust.rs
+++ libcasr/src/rust.rs
@@ -9,23 +9,16 @@ pub struct RustPanic;
 
 impl Exception for RustPanic {
     fn parse_exception(stderr: &str) -> Option<ExecutionClass> {
-        let stderr_list: Vec<String> = stderr
-            .split('\n')
-            .map(|l| l.trim_end().to_string())
-            .collect();
-        let rexception = Regex::new(r"thread '.+?' panicked at '(.+)?'").unwrap();
-        let Some(pos) = stderr_list
-            .iter()
-            .position(|line| rexception.is_match(line))
-        else {
+        let rexception = Regex::new(r"thread '.+?' panicked at (?:'(.*)'|.+?:\n(.*))").unwrap();
+        let Some(captures) = rexception
+            .captures(stderr) else {
             return None;
         };
-        let message = rexception
-            .captures(&stderr_list[pos])
-            .unwrap()
-            .get(1)
-            .unwrap()
-            .as_str();
+        let message = if let Some(message) = captures.get(1) {
+            message.as_str()
+        } else {
+            captures.get(2).unwrap().as_str()
+        };
         Some(ExecutionClass::new((
             "NOT_EXPLOITABLE",
             "RustPanic",
