7735af04baed71286c6833590d10b8cdb3f6148a
fedotoff <fedotoff@ispras.ru>
2023-09-05 13:28:18 +0300

    Fix bail

--- casr/src/bin/casr-cluster.rs
+++ casr/src/bin/casr-cluster.rs
@@ -244,7 +244,7 @@ fn deduplication(indir: &Path, outdir: Option<PathBuf>, jobs: usize) -> Result<(
     before += paths.len();
 
     if after == 0 {
-        bail!("All {before} CASR reports are corrupted");
+        bail!("All {} CASR reports are corrupted", before);
     }
 
     Ok((before, after))
