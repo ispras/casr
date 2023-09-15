e08cf5daf2e3faa805800709f1f79c6173851398
fedotoff <fedotoff@ispras.ru>
2023-09-15 17:42:29 +0300

    Add initial corpus and extract crashline

--- libcasr/fuzz/fuzz_targets/parse_stacktrace.rs
+++ libcasr/fuzz/fuzz_targets/parse_stacktrace.rs
@@ -2,47 +2,71 @@
 
 use libfuzzer_sys::fuzz_target;
 
-use libcasr::asan::AsanStacktrace;
-use libcasr::gdb::GdbStacktrace;
-use libcasr::go::GoStacktrace;
-use libcasr::java::JavaStacktrace;
-use libcasr::python::PythonStacktrace;
-use libcasr::stacktrace::ParseStacktrace;
+use libcasr::{
+    asan::AsanStacktrace,
+    constants::{
+        STACK_FRAME_FILEPATH_IGNORE_REGEXES_CPP, STACK_FRAME_FILEPATH_IGNORE_REGEXES_GO,
+        STACK_FRAME_FILEPATH_IGNORE_REGEXES_JAVA, STACK_FRAME_FILEPATH_IGNORE_REGEXES_PYTHON,
+        STACK_FRAME_FILEPATH_IGNORE_REGEXES_RUST, STACK_FRAME_FUNCTION_IGNORE_REGEXES_CPP,
+        STACK_FRAME_FUNCTION_IGNORE_REGEXES_GO, STACK_FRAME_FUNCTION_IGNORE_REGEXES_JAVA,
+        STACK_FRAME_FUNCTION_IGNORE_REGEXES_PYTHON, STACK_FRAME_FUNCTION_IGNORE_REGEXES_RUST,
+    },
+    gdb::GdbStacktrace,
+    go::GoStacktrace,
+    init_ignored_frames,
+    java::JavaStacktrace,
+    python::PythonStacktrace,
+    stacktrace::{
+        CrashLineExt, ParseStacktrace, STACK_FRAME_FILEPATH_IGNORE_REGEXES,
+        STACK_FRAME_FUNCTION_IGNORE_REGEXES,
+    },
+};
 
 fuzz_target!(|data: &[u8]| {
     if data.len() < 2 {
         return;
     }
     let s = String::from_utf8_lossy(&data[1..]);
+    init_ignored_frames!("cpp", "rust", "python", "go", "java");
     match data[0] % 5 {
         0 => {
             // Asan
-            if let Ok(st) = AsanStacktrace::extract_stacktrace(&s) {
-                let _ = AsanStacktrace::parse_stacktrace(&st);
+            if let Ok(raw) = AsanStacktrace::extract_stacktrace(&s) {
+                if let Ok(st) = AsanStacktrace::parse_stacktrace(&raw) {
+                    let _ = st.crash_line();
+                }
             }
         }
         1 => {
             // Go
-            if let Ok(st) = GoStacktrace::extract_stacktrace(&s) {
-                let _ = GoStacktrace::parse_stacktrace(&st);
+            if let Ok(raw) = GoStacktrace::extract_stacktrace(&s) {
+                if let Ok(st) = GoStacktrace::parse_stacktrace(&raw) {
+                    let _ = st.crash_line();
+                }
             }
         }
         2 => {
             // Python
-            if let Ok(st) = PythonStacktrace::extract_stacktrace(&s) {
-                let _ = PythonStacktrace::parse_stacktrace(&st);
+            if let Ok(raw) = PythonStacktrace::extract_stacktrace(&s) {
+                if let Ok(st) = PythonStacktrace::parse_stacktrace(&raw) {
+                    let _ = st.crash_line();
+                }
             }
         }
         3 => {
             // Java
-            if let Ok(st) = JavaStacktrace::extract_stacktrace(&s) {
-                let _ = JavaStacktrace::parse_stacktrace(&st);
+            if let Ok(raw) = JavaStacktrace::extract_stacktrace(&s) {
+                if let Ok(st) = JavaStacktrace::parse_stacktrace(&raw) {
+                    let _ = st.crash_line();
+                }
             }
         }
         _ => {
             // Gdb
-            if let Ok(st) = GdbStacktrace::extract_stacktrace(&s) {
-                let _ = GdbStacktrace::parse_stacktrace(&st);
+            if let Ok(raw) = GdbStacktrace::extract_stacktrace(&s) {
+                if let Ok(st) = GdbStacktrace::parse_stacktrace(&raw) {
+                    let _ = st.crash_line();
+                }
             }
         }
     }
